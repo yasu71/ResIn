fdafc6c165786fcae1d01c6944846e257ffcb372
Yasu <yasu71@gmail.com>
2020-04-05 18:07:17 +0000

    added login route

--- server.js
+++ server.js
@@ -35,11 +35,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const resourcesRoutes = require("./routes/resources");
+const loginRoutes = require("./routes/login");
+
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/users", usersRoutes(db));
 app.use("/resources", resourcesRoutes(db));
+app.use("/login", loginRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
