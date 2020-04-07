45ec58131e6dada4b5c3b0860aa352a8ee1cd9b1
Jaspinder Sabharwal <jas@jaspinder.net>
2020-04-07 16:41:51 +0000

    added resources to main page

--- public/scripts/components/searchform.js
+++ public/scripts/components/searchform.js
@@ -21,13 +21,21 @@ $(() => {
 
   const createResElement = function(resource) {
     const $article = $('<article>').addClass('loaded-resource');
-    const $title = $('<p>').text(resource.title);
-    const $url = $('<p>').text(resource.url);
-    const $description = $('<p>').text(resource.description);
-    const $rating = $('<p>').text(resource.rating);
+    const $title = $('<p>').text(resource.title).addClass('resource-title');
+    const $image = $('<img>').attr("src", resource.img_url).addClass('img-url');
+    const $url = $('<p>').text(resource.url).addClass('new-url');
+    const $description = $('<p>').text(resource.description).addClass('resource-desc');
+    const $rating = $('<div>').addClass('starrr').attr({
+      id: 'star1',
+    }).starrr();
+    //const $rating = $('<p>').text(resource.rating + '/5 rating').addClass('resource-rating');
+    const $footer = $('<p>').addClass('resource-footer');
+    const $footerspan = $('<span>').addClass('icons');
     const $heart = $('<i>').addClass('fa fa-heart');
 
-    $article.append($title, $url, $description, $rating, $heart);
+    $footerspan.append($heart);
+    $footer.append($rating, $footerspan);
+    $article.append($image, $title, $url, $description, $footer);
 
     return $article;
 
---
+++ routes/resources2.js
@@ -0,0 +1,77 @@
+// const express = require('express');
+// const router  = express.Router();
+// const cookieSession = require('cookie-session');
+
+// router.use(cookieSession({
+//   name: 'session',
+//   keys: ['key1']
+// }));
+
+// module.exports = (db) => {
+
+//   // My Resources Page for User once logged in
+//   router.get("/user/:userid", (req, res) => {
+//     console.log('Resources Get Returned');
+//     db.query(`SELECT resources.*, ratings.* FROM resources JOIN ratings ON resources.id = resource_id WHERE resources.user_id = ${req.params.userid};`)
+//       .then(data => {
+//         const resources = data.rows;
+//         res.json({ resources });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
+
+//   // Get request for the search feature,  search will convert table data and input to lowercase to compare before returning results to the searchform.js
+//   router.get("/search", (req, res) => {
+
+//     db.query(`SELECT resources.*, ratings.* FROM resources JOIN ratings ON resources.id = resource_id WHERE LOWER(resources.title) LIKE LOWER('%${req.query.search}%') OR resources.description LIKE LOWER('%${req.query.search}%')`)
+//       .then(data => {
+//         const resources = data.rows;
+//         res.json({ resources });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
+
+//   router.post("/", (req, res) => {
+
+//     // If user is not logged in the user will be redirected to login page and code will stop executing.
+//     const userId = req.session.user_id;
+//     if (!userId) {
+//       res.redirect('/login');
+//       return;
+//     }
+
+//     // The rest of this code will execute having passed login credentials
+//     const resourceUrl = req.body.url;
+//     const resourceTitle = req.body.title;
+//     const resourceDescription = req.body.description;
+//     const resourceCategory = 1; // <-- change to category and see how we can check if this category already exists in the category table *********
+
+//     console.log(req.body);
+//     if (!resourceDescription || !resourceTitle || !resourceUrl) { // <---- need to add userid and category to this one req.body as been added to these variables *********
+
+//       res.status(400).json({ error: 'invalid request: no data in POST body'});
+//     } else {
+//       db.query(`INSERT INTO resources (user_id, url, title, description, category_id) VALUES (${userId}, ${resourceUrl}, ${resourceTitle}, ${resourceDescription}, ${resourceCategory})`)
+//         .then(data => {
+//           const resources = data.rows;
+//           res.json({ resources });
+//         })
+//         .catch(err => {
+//           res
+//             .status(500)
+//             .json({ error: err.message });
+//         });
+//     }
+//   });
+
+//   return router;
+// };
+
